447d6b1b41135a60066e87f9de3a6ba79caa192e
hassan <prohyatm@gmail.com>
2026-01-06 17:25:02 +0200

    test

--- BackEnd/DVLD.DataAccessLayer/GenericAdoMethods.cs
+++ BackEnd/DVLD.DataAccessLayer/GenericAdoMethods.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.Data.SqlClient;
+using System;
 namespace DVLD.DataAccessLayer.GeneralAdoMethods
+    //git add
 {
     public static  class GenericAdoMethods
     {
         public static DateOnly GetDateOnly(this SqlDataReader reader, string column)
         {
             return DateOnly.FromDateTime(
-                reader.GetDateTime(reader.GetOrdinal(column))
+                reader.GetDateTime(reader.GetOrdinal(column)) 
             );
         }
     }
